4682e312e65b34604c3211320c13b01380c9c644
Hamza Ghelle <hamzaghellegmail.com>
2021-09-15 13:12:51 -0400

    Check if arrays match

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`The '${actual}' and the '${expected}' are the same✅`);
+  } else console.assert(actual === expected, "❌");
+};
+
+const eqArrays = function (array1, array2) {
+
+
+  for (let i = 0; i < array1.length; i++ ) {
+    if (array1[i] !== array2[i]) {
+      return false; 
+  } else return true;
+
+  }
+}
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
