603b1a122d33ff1c887c47daba10177be71067c0
Hamza Ghelle <hamzaghellegmail.com>
2021-09-20 22:32:45 -0400

    test using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle  = require('../middle');
+describe('#middle', () => {
+  it("returns [2] for [1, 2, 3,]", () => {
+    assert.deepEqual(middle([1,2,3]), [2])
+  });
 
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-middle(numbers);
-assertArraysEqual([2], middle([1, 2, 3])) // => [2]
-assertArraysEqual([1,2], middle([1, 2, 3, 4, 5]));
+  it("returns [2, 3] for [1, 2, 3, 4]",() => {
+    assert.deepEqual(middle([1,2,3,4]), [2, 3])
+  });
+  it("returns [1] for [1]", () => {
+    assert.deepEqual(middle([1]), [1])
+  });
+
+})
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,29 @@
-const assertEqual = require('../assertEquals');
-const tail = require('../tail.js');
+const assert = require('chai').assert;
+const tail  = require('../tail');
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe('#tail', () => {
+  it('returns [dog, cat] of [fish, dog, cat]', () =>{
+    assert.deepEqual(tail(['fish', 'dog', 'cat']), ['dog', 'cat']);
+  })
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it('returns [1, 2] of [3, 1, 2]', () =>{
+    assert.deepEqual(tail([3, 1, 2]), [1, 2]);
+  })
+})
+
+
+
+
+// const tail = require('../tail.js');
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
